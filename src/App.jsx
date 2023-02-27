0fda6e0a0d0f9ff292cd5b4efea435ff39a7f6f9
grayTurtle01 <the.correo@protonmail.com>
2023-02-27 05:04:51 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+
+
+import './App.css'
+
+function Home(){
+    return(
+        <div>
+            <h1>Home</h1>
+        </div>
+    )
+}
+
+function About(){
+    return(
+        <div>
+            <h1>About</h1>
+        </div>
+    )
+}
+
+function Vans(){
+    return(
+        <div>
+            <h1>Vans</h1>
+        </div>
+    )
+}
+
+function NavBar(){
+    return(
+        <nav>
+
+            <Link to={'/'} className='logo' >#App</Link>
+            <Link to={'/about'}>About</Link>
+            <Link to={'/vans'}>Vans</Link>
+        </nav>
+    )
+}
+
+function Footer(){
+    return(
+        <footer className='vans--footer'>
+         @ 2023 App
+        </footer>
+    )
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+        
+        <NavBar />
+        
+        <Routes>
+            <Route path='/'  element={<Home />}/>
+            <Route path='/about'  element={<About />}/>
+            <Route path='/vans'  element={<Vans />}/>
+        </Routes>
+
+
+        <Footer />
+
+    </BrowserRouter>
+  )
+}
+
+export default App
