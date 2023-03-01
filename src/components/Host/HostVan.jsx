6ffa79b29099477c6ce751a7cfb6651ef5391abb
grayTurtle01 <the.correo@protonmail.com>
2023-03-01 08:06:16 -0600

    add: Host-Van-Details component

--- src/components/Host/HostVan.jsx
+++ src/components/Host/HostVan.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import {  NavLink, useParams } from 'react-router-dom'
 
 function HostVan() {
+    const [van, setVan] = useState({})
+
     let parameters = useParams()
 
+    useEffect( () => {
+        fetch(`/api/host/vans/${parameters.id}`)
+            .then( res => res.json() )
+            .then( data => {
+                setVan(data.vans[0]);
+            })
+    }, [])
+
+    let link_style = {
+        fontWeight: 'bold',
+        textDecoration: 'underline'
+    }
+    
+    const foo = ({isActive}) => isActive? link_style: {}
+    
+
     return (
-        <div>
-            <h1>HostVan:  {parameters.id}</h1>
+        <div className='host-van-details-card'>
+
+            <header className='host-van-details-header'>
+                <img src={van.imageUrl} />
+                
+                <div>
+                    <button className='host-van-detail-button'>{van.type}</button>
+                    <h1>{van.name}</h1>
+                    <p><b>${van.price}</b>/day</p>
+                </div>
+            </header>
+
+            <footer>
+                <NavLink style={ foo }to={'/host/vans/:id/details'}>Details</NavLink>
+                <NavLink style={ foo }to={'/host/vans/:id/pricing'}>Pricing</NavLink>
+                <NavLink style={ foo }to={'/host/vans/:id/photos'}>Photos</NavLink>
+            </footer>
+
         </div>
     )
 }
--- src/components/Host/HostVans.jsx
+++ src/components/Host/HostVans.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 import './Host.css'
 
 function HostVans() {
     const [vans, setVans] = useState([])
 
-    useEffect( () => {
+    useEffect(() => {
         fetch('/api/host/vans')
-            .then( res => res.json())
-            .then( data => {
+            .then(res => res.json())
+            .then(data => {
                 setVans(data.vans);
             })
     }, [])
@@ -17,15 +18,20 @@ function HostVans() {
         <div>
             <h1>Your listed vans</h1>
             {
-                vans.map( van => {
-                    return <div key={van.id} className='host-vans-card'>
-                            <img src={van.imageUrl} />
+                vans.map(van => {
 
-                            <div>
-                                <h2>{van.name}</h2>
-                                <span>${van.price}/day</span>
-                            </div>
-                          </div>
+                    return (
+                        <Link to={`/host/vans/${van.id}`} style={{ color: 'black', textDecoration:'none'}} >
+                            <div key={van.id} className='host-vans-card'>
+                                <img src={van.imageUrl} />
+
+                                <div>
+                                    <h2>{van.name}</h2>
+                                    <span>${van.price}/day</span>
+                                </div>
+                            </div> 
+                        </Link>
+                        )
                 })
             }
         </div>
