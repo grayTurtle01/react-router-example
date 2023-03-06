d89d6a22ebb34d7ad8e4b04e2f56beadf41c436d
grayTurtle01 <the.correo@protonmail.com>
2023-03-06 06:36:45 -0600

    update: HomePage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import imagen from '../assets/home-hero.png'
 
 export function Home() {
 
     return (
         <div className='home'>
-            <h1>Home</h1>
+            <h1>You got the travel plans, we got the travel vans.</h1>
+            <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
             <img src={imagen} className='home-image' />
+            <Link to="vans">Find your van</Link>
         </div>
     );
 }
